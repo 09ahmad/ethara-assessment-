36ff53df575bfdef48f0484822f165b6f436a40e
09ahmad <shkhahmad64@gmail.com>
2026-05-01 16:22:42 +0530

    pushing hardcoded backend url on frontend

--- backend/src/docs/swagger.ts
+++ backend/src/docs/swagger.ts
@@ -11,7 +11,7 @@ const options: swaggerJsdoc.Options = {
     },
     servers: [
       {
-        url: "http://localhost:3000/api/v1",
+        url: `${process.env.BASE_URL || "http://localhost:3000"}/api/v1`,
         description: "Local development server",
       },
     ],
--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ?? "http://localhost:3000/api/v1";
+console.log(import.meta.env.VITE_API_BASE_URL);
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ?? "https://ethara-assessment-production.up.railway.app/api/v1";
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
